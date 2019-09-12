f31dcf108836877f718cf33e2a31950447f515f2
Joshuha <jthomasw>
2019-09-12 10:34:09 +0200

    updated binning

--- plotter/make_ratios.py
+++ plotter/make_ratios.py
@@ -29,6 +29,13 @@ features={
 "gen_lepton1_pt":{"nbin":20,"min":0.5,"max":500.5,"cut":"EVENT_event*(n_gen_jets>=4)","xlabel":"gen_lepton1_pt"},
 "gen_lepton2_pt":{"nbin":20,"min":0.5,"max":500.5,"cut":"EVENT_event*(n_gen_jets>=4)","xlabel":"gen_lepton2_pt"}
 }
+binning={
+"MCGenHTall":[0,120,180,240,300,360,440,540,680,900,1500],
+"MCGenHThad":[0,90,140,180,240,300,380,460,540,650,850,1500],
+"MCGenHTlep":[0,20,50,80,110,150,200,300,400,550,800],
+"MCGenMET":[0,20,50,80,120,180,300,500,1200],
+"_lepton_":[0,20,25,33,45,60,80,110,160,500]
+}
 
 nominal_weights = {'genWeight':'EVENT_genWeight'}
 systematics=["nominal","genWeight"]
