28c220db8db0950ce6b4be5cd20245dd137a1b77
Joshuha <jthomasw>
2019-09-27 16:13:50 +0200

    added additional variables for comparison

--- plotter/make_ratios.py
+++ plotter/make_ratios.py
@@ -11,8 +11,8 @@ treename = "TNT/BOOM";
 
 region_ = {
 "2lss_1bgeq4j":"(n_gen_jets>=4 && n_gen_bjets==1)",
-"2lss_1beeq3j":"(n_gen_jets==3 && n_gen_bjets==1)"
-#"2lss_2bgeq4j":"(n_gen_jets>=4 && n_gen_bjets>=2)",
+#"2lss_1beeq3j":"(n_gen_jets==3 && n_gen_bjets==1)"
+"2lss_2bgeq4j":"(n_gen_jets>=4 && n_gen_bjets>=2)",
 #"2lss_2beeq3j":"(n_gen_jets==3 && n_gen_bjets>=2)"
 }
 
