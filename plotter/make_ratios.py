174c1276293517343b15fea638a05461ec6ed193
Joshuha <jthomasw>
2019-09-30 21:41:48 +0200

    change scaling

--- plotter/make_ratios.py
+++ plotter/make_ratios.py
@@ -189,9 +189,9 @@ def plotSysts():
                 #hist_eventweights = inputfile.Get("EventWeight")
                 if normalization:
                     hist_nom.Scale(1./hist_nom.Integral())
-                # Need to scale down to 1/XS*BR*Lumi = 7040.32 to match ATLAS fiducial normalisation
+                # Need to scale down to 1/XS*BR*Lumi = 1/3891190 to match ATLAS fiducial normalisation
                 # Scale up to correct ttW inclusive XS = 600.8
-                hist_nom.Scale(600.8/232785)
+                hist_nom.Scale(600.8/3891190)
                 h_ratio = createRatio(hist_nom, hist_nom, values["xlabel"])
                 c, pad1, pad2 = createCanvasPads()
                 hist_vars = []
@@ -222,7 +222,7 @@ def plotSysts():
                         hist_var.SetLineWidth(3)
                         if normalization:
                             hist_var.Scale(1./hist_var.Integral())
-                        hist_var.Scale(600.8/232785)
+                        hist_var.Scale(600.8/3891190)
                         hist_vars.append(hist_var)
                         h_ratio_var = createRatio(hist_var, hist_nom ,values["xlabel"])
                         hist_ratio_vars.append(h_ratio_var)
@@ -241,7 +241,7 @@ def plotSysts():
                             hist_var.SetLineWidth(3)
                             if normalization:
                                 hist_var.Scale(1./hist_var.Integral())
-                            hist_var.Scale(600.8/232785)
+                            hist_var.Scale(600.8/3891190)
                             hist_vars.append(hist_var)
                             h_ratio_var = createRatio(hist_var, hist_nom,values["xlabel"])
                             hist_ratio_vars.append(h_ratio_var)
