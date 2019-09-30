24fbe61316d0c4090d846452af30c0ce25c59b9a
Joshuha <jthomasw>
2019-09-30 16:31:09 +0200

    edited make_ratios region selection

--- plotter/make_ratios.py
+++ plotter/make_ratios.py
@@ -10,8 +10,8 @@ inputDirectories = ["/publicfs/cms/data/TopQuark/ttV-modelling/condor/ttH2016All
 treename = "TNT/BOOM";
 
 region_ = {
-"2lss_1bgeq4j":"(n_gen_jets>=4 && n_gen_bjets>=1)",
-"2lss_1beeq3j":"(n_gen_jets==3 && n_gen_bjets>=1)",
+"2lss_1bgeq4j":"(n_gen_jets>=4 && n_gen_bjets==1)",
+"2lss_1beeq3j":"(n_gen_jets==3 && n_gen_bjets==1)",
 "2lss_2bgeq4j":"(n_gen_jets>=4 && n_gen_bjets>=2)",
 "2lss_2beeq3j":"(n_gen_jets==3 && n_gen_bjets>=2)"
 }
@@ -279,11 +279,11 @@ def plotSysts():
                     hist_vars.append(hist_atlas_scaleUp)
                     hist_vars.append(hist_atlas_scaleDown)
                     h_ratio_atlas = createRatio(hist_atlas,hist_nom,values["xlabel"])
-                    hist_atlas_scaleUp = createRatio(hist_atlas_scaleUp,hist_nom,values["xlabel"])
-                    hist_atlas_scaleDown = createRatio(hist_atlas_scaleDown,hist_nom,values["xlabel"])
+                    h_ratio_atlas_scaleUp = createRatio(hist_atlas_scaleUp,hist_nom,values["xlabel"])
+                    h_ratio_atlas_scaleDown = createRatio(hist_atlas_scaleDown,hist_nom,values["xlabel"])
                     hist_ratio_vars.append(h_ratio_atlas)
-                    hist_ratio_vars.append(hist_atlas_scaleUp)
-                    hist_ratio_vars.append(hist_atlas_scaleDown)
+                    hist_ratio_vars.append(h_ratio_atlas_scaleUp)
+                    hist_ratio_vars.append(h_ratio_atlas_scaleDown)
 
                     # draw everything
                     pad1.cd()
