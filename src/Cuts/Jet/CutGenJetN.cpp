25b9afb9df62bc59116b442e0c4e1ccda5cd5d30
Joshuha <jthomasw>
2019-09-30 10:43:08 +0200

    Revert "updated HEP submission script to more generic cuts"
    This reverts commit dc852ad4093f56c5773f9e76686fa8d9526a0387.

--- src/Cuts/Jet/CutGenJetN.cpp
+++ src/Cuts/Jet/CutGenJetN.cpp
@@ -138,7 +138,7 @@ Bool_t CutGenJetN::Apply()
   Bool_t JetNumberMaxPass    = kTRUE;
   Bool_t JetDeltaRPass    = kTRUE;
 
-  // Assign objects in vectors
+  // Cut on Min Pt of Jet
   std::vector<MCJet> jetVector;
   jetVector.assign(evObj -> MCJets.begin(), evObj -> MCJets.end());
   std::vector<MCElectron> EleVector;
@@ -146,9 +146,7 @@ Bool_t CutGenJetN::Apply()
   std::vector<MCMuon> MuVector;
   MuVector.assign(evObj -> MCMuons.begin(), evObj -> MCMuons.end());
 
-  // Loop over all jets
   for (auto const jet : jetVector){
-    // Jet pT > minimum pT requirement
     if (jet.Pt() > LeadingJetPt) LeadingJetPt = jet.Pt();
     if (jet.Pt() > _JetPtCutMin && std::abs(jet.Eta()) < 2.5 ){
       for (auto const lep : EleVector){ if (jet.DeltaR(lep)<0.4){ JetDeltaRPass = kFALSE; } }
